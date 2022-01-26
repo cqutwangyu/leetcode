aed38e9d37c2b0dc4e1ea620a7037817805be97f
cqutwangyu <1903372529@qq.com>
2022-01-26 23:23:39 +0800

    feat(leetcode):617.合并二叉树

---
+++ src/com/company/leetcode/domain/TreeNode.java
@@ -0,0 +1,26 @@
+package com.company.leetcode.domain;
+
+/**
+ * todo desc
+ *
+ * @author 王渔
+ * @date 2022/1/26 21:48
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
