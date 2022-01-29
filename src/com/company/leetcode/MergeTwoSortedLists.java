112776d2f6c512fada443fe9473ad22477084998
cqutwangyu <cqwangyu@2016.cqut.edu.cn>
2022-01-29 21:28:46 +0800

    feat(leetcode):21.合并两个有序链表

---
+++ src/com/company/leetcode/MergeTwoSortedLists.java
@@ -0,0 +1,62 @@
+package com.company.leetcode;
+
+import com.company.leetcode.domain.ListNode;
+
+/**
+ * 21.合并两个有序链表
+ * <p>
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100 <= Node.val <= 100
+ * l1 和 l2 均按 非递减顺序 排列
+ * <p>
+ * Related Topics 递归 链表 👍 2166 👎 0
+ *
+ * @author 王渔
+ * @date 2022/1/29 21:04
+ */
+public class MergeTwoSortedLists {
+    class Solution {
+        /**
+         * 将两个升序链表按升序合并，并返回链表
+         * 比较两个链表的节点大小，那个节点值小，就取那个节点
+         * 被添加过的节点则指向自己的下一个节点，进入下一次比较
+         * 利用递归的压栈特性，最终返回结果是第一次入栈时所需要return的那个链表
+         */
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            if (l1 == null) {
+                return l2;
+            }
+            if (l2 == null) {
+                return l1;
+            }
+
+            if (l1.val < l2.val) {
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
+            } else {
+                l2.next = mergeTwoLists(l1, l2.next);
+                return l2;
+            }
+        }
+    }
+}
